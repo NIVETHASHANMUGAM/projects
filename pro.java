4c819ea61f8f17cbe182dbba348a1cf9c54cb2e7
NIVETHASHANMUGAM <nivethaabc@gmail.com>
2016-08-09 16:08:13 +0530

    Create pro.java

---
+++ pro.java
@@ -0,0 +1,18 @@
+class test
+{
+public static void main()
+{
+Scanner n=new scanner();
+int s=n.nextInt();
+if(s>0)
+System.out.println("positive");
+else if(s<0)
+{
+System.out.println("negative");
+}
+else if(s==0)
+{
+System.out.println("zero");
+}
+}
+}
